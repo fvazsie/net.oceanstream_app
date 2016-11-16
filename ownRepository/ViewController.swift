5bf47c1e8cfc3a5b17af08d285e03e45c9b6c6a0
David Vázquez A <davidva@MacBook-Pro-de-David.local>
2016-11-16 17:22:55 -0600

    Initial Commit

---
+++ ownRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ownRepository
+//
+//  Created by David Vázquez A. on 16/11/16.
+//  Copyright © 2016 net.oceanstream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
